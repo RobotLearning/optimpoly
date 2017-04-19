d4bedd8acd5fff131c35d37d8871085864b1bdbc
okankoc <okankoc@gmail.com>
2017-04-19 02:35:05 +0200

    Turned off spin based prediction. Tested with SL and spin-mismatch-case now also works!! (meaning we can return balls with MPC).

--- src/player/table_tennis.cpp
+++ src/player/table_tennis.cpp
@@ -724,7 +724,7 @@ static void table_contact_model(const double & CFTX, const double & CFTY, const 
  */
 vec calc_next_ball(const vec & xnow, double dt) {
 
-	TableTennis tennis = TableTennis(xnow,true,false);
+	TableTennis tennis = TableTennis(xnow,false,false);
 	tennis.integrate_ball_state(dt);
 	static vec6 out = zeros<vec>(6);
 	out(span(X,Z)) = tennis.ball_pos;
@@ -748,7 +748,7 @@ vec calc_next_ball(const vec & xnow, double dt) {
  */
 vec calc_next_ball(const racket & robot, const vec & xnow, double dt) {
 
-	TableTennis tennis = TableTennis(xnow,true,false);
+	TableTennis tennis = TableTennis(xnow,false,false);
 	tennis.integrate_ball_state(robot,dt);
 	static vec6 out = zeros<vec>(6);
 	out(span(X,Z)) = tennis.ball_pos;
