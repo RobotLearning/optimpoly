a46c9e231fa83b790d13033ef704d40f7a65ab42
Okan Koc <okoc@sill.is.localnet>
2018-01-09 18:19:14 +0100

    Including a new test for generating runtime histograms for FP and DP over 500 trials.

--- test/test_optim.cpp
+++ test/test_optim.cpp
@@ -59,6 +59,35 @@ inline void init_right_posture(vec7 & q0) {
 }
 
 /*
+ * Initialize robot posture
+ */
+void init_posture(vec7 & q0, int posture, bool verbose) {
+
+	rowvec qinit;
+	switch (posture) {
+	case 2: // right
+		if (verbose)
+			cout << "Initializing robot on the right side.\n";
+		qinit << 1.0 << -0.2 << -0.1 << 1.8 << -1.57 << 0.1 << 0.3 << endr;
+		break;
+	case 1: // center
+		if (verbose)
+			cout << "Initializing robot on the center.\n";
+		qinit << 0.0 << 0.0 << 0.0 << 1.5 << -1.75 << 0.0 << 0.0 << endr;
+		break;
+	case 0: // left
+		if (verbose)
+			cout << "Initializing robot on the left side\n";
+		qinit << -1.0 << 0.0 << 0.0 << 1.5 << -1.57 << 0.1 << 0.3 << endr;
+		break;
+	default: // default is the right side
+		qinit << 1.0 << -0.2 << -0.1 << 1.8 << -1.57 << 0.1 << 0.3 << endr;
+		break;
+	}
+	q0 = qinit.t();
+}
+
+/*
  *
  * Here testing NLOPT optimization for VHP player
  *
@@ -199,6 +228,81 @@ BOOST_AUTO_TEST_CASE(test_dp_optim) {
 	BOOST_TEST(update);
 }
 
+/**
+ * Evaluate time efficiency of FP and DP optimizations over hundreds of tests
+ */
+BOOST_AUTO_TEST_CASE( test_time_efficiency ) {
+
+	// For FP or DP
+	// initialize ball randomly each time
+	// measure time elapsed for each and record in a histogram
+	int num_trials = 500;
+	BOOST_TEST_MESSAGE("Testing time efficiency of FP & DP on " << num_trials << " instances.");
+	double Tmax = 1.0, lb[2*NDOF+1], ub[2*NDOF+1];
+	set_bounds(lb,ub,0.01,Tmax);
+	joint qact;
+	spline_params poly;
+	TableTennis tt = TableTennis(true,false);
+	arma_rng::set_seed_random();
+	double std_noise = 0.0001;
+	double std_model = 0.3;
+	int ball_launch_side;
+	int joint_init_pose;
+	optim_des racket_params;
+	int N = 1000;
+	racket_params.Nmax = 1000;
+	double time_land_des = 0.8;
+	vec2 ball_land_des = {0.0, dist_to_table - 3*table_length/4};
+	mat balls_pred = zeros<mat>(6,N);
+	vec time_elapsed_init = zeros<vec>(num_trials);
+	vec time_elapsed_run_fp = zeros<vec>(num_trials);
+	vec time_elapsed_dp_lookup = zeros<vec>(num_trials);
+	vec time_elapsed_dp_no_lookup = zeros<vec>(num_trials);
+    wall_clock timer;
+
+	for (int n = 0; n < num_trials; n++) { // for each trial
+		std::cout << "Trial: " << n+1 << std::endl;
+		ball_launch_side = (randi(1,distr_param(0,2)).at(0));
+		joint_init_pose = (randi(1,distr_param(0,2)).at(0));
+		init_posture(qact.q,joint_init_pose,false);
+		tt.reset_stats();
+		tt.set_ball_gun(0.05,ball_launch_side);
+		for (int i = 0; i < N; i++) {
+			tt.integrate_ball_state(DT);
+			balls_pred.col(i) = tt.get_ball_state();
+		}
+		timer.tic();
+		racket_params = calc_racket_strategy(balls_pred,ball_land_des,time_land_des,racket_params);
+		FocusedOptim fp = FocusedOptim(qact.q.memptr(),lb,ub);
+		fp.set_verbose(false);
+		fp.set_des_params(&racket_params);
+		fp.update_init_state(qact);
+		time_elapsed_init(n) = timer.toc() * 1e3;
+		timer.tic();
+		fp.run();
+		time_elapsed_run_fp(n) = timer.toc() * 1e3;
+		/*DefensiveOptim dp = DefensiveOptim(qact.q.memptr(),lb,ub,true,false);
+		dp.set_verbose(false);
+		dp.set_des_params(&racket_params);
+		dp.update_init_state(qact);
+		timer.tic();
+		dp.run();
+		time_elapsed_dp_no_lookup(n) = timer.toc() * 1e3;*/
+		DefensiveOptim dp2 = DefensiveOptim(qact.q.memptr(),lb,ub,true,true);
+		dp2.set_verbose(false);
+		dp2.set_des_params(&racket_params);
+		dp2.update_init_state(qact);
+		timer.tic();
+		dp2.run();
+		time_elapsed_dp_lookup(n) = timer.toc() * 1e3;
+	}
+	umat hists = zeros<umat>(10,4);
+	hists.col(0) = hist(time_elapsed_init, linspace<vec>(0,9,10));
+	hists.col(1) = hist(time_elapsed_run_fp, linspace<vec>(10,100,10));
+	hists.col(2) = hist(time_elapsed_dp_no_lookup, linspace<vec>(10,10,10));
+	hists.col(3) = hist(time_elapsed_dp_lookup, linspace<vec>(10,60,10));
+	hists.save("histograms.txt", raw_ascii);
+}
 
 
 /*
