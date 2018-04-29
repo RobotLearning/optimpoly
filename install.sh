05cdf5068b7cf7e32a041eff795cd8793546044a
okankoc <okankoc@gmail.com>
2018-04-29 19:26:24 +0200

    Creating install file to be run in a subshell Compiles in release mode inside build/release Install in lib/ and ./ for unit tests Runs the unit tests with commands

---
+++ install.sh
@@ -0,0 +1,18 @@
+# IMPORTANT NOTE: run in a subshell for cd commands: 
+# . install.sh
+
+cd build/release
+cmake -DCMAKE_BUILD_TYPE=Release ../..
+make
+make install
+cd ../..
+./unit_tests --log_level=message --show_progress=yes
+
+# FOR OLD SL
+#cp SL_code/ilc_task.c ~/robolab/barrett/src/ilc_task.c
+
+# FOR NEW SL COPY EVERYTHING INCLUDING CMAKELIST AND README
+#cp SL_code/*.c ~/sl_xeno/barrett/src/learning_control/*.c
+#cp SL_code/*.h ~/sl_xeno/barrett/include/*.h
+#cp CMakeLists.txt ~/sl_xeno/barrett/src/learning_control/CMakeLists.txt
+#cp readme.txt ~/sl_xeno/barrett/src/learning_control/readme.txt
