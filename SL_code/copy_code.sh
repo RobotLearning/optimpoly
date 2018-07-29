2d64fb884b3eac70034a051118d62bf53528825b
Sebastian Gomez <sgomez@tue.mpg.de>
2018-07-29 20:29:56 +0200

    Adding the red and the black racket datasets for vision detection experiments

--- SL_code/copy_code.sh
+++ SL_code/copy_code.sh
@@ -7,9 +7,11 @@ cp SL_code/grav_comp/grav_comp.c ~/projects/sl_xeno/barrett/src/grav_comp/
 cp SL_code/grav_comp/CMakeLists.txt ~/projects/sl_xeno/barrett/src/grav_comp/
 cp SL_code/player/table_tennis_task.c ~/projects/sl_xeno/barrett/src/table-tennis/
 
-# then to BORIS SL with custom inverse dynamics
+# then to BORIS SL with custom inverse dynamics into sl_wam
 cp SL_code/serve/CMakeLists.txt ~/sebastian/sl_xeno/sl_wam/src/serve/
 cp SL_code/serve/serve_task.c ~/sebastian/sl_xeno/sl_wam/src/serve/
 cp SL_code/grav_comp/grav_comp.c ~/sebastian/sl_xeno/sl_wam/src/grav_comp/
 cp SL_code/grav_comp/CMakeLists.txt ~/sebastian/sl_xeno/sl_wam/src/grav_comp/
 cp SL_code/player/table_tennis_task.c ~/sebastian/sl_xeno/sl_wam/src/table-tennis/
+
+# and also into barrett of BORIS SL?
